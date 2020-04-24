df8c911237706f7eb5dd86f7990d195abf690109
mysk <wolfbot.owner@gmail.com>
2020-04-24 18:04:14 +0900

    fix(output): fix check status message

--- domain/output/village.go
+++ domain/output/village.go
@@ -12,13 +12,17 @@ type VillageCheckStatus struct {
 
 func (o VillageCheckStatus) StringForHuman() string {
 	if o.VillageNotExist {
-		fmt.Sprintf("村が作成されていません")
+		return `○現在の状況
+村が作成されていません
+
+○ヘルプ
+このグループで人狼ゲームを行う場合は、
+＠村作成
+と入力してください`
 	}
 
-	return fmt.Sprintf(`
-○現在の状況
-%v
-`,
+	return fmt.Sprintf(`○現在の状況
+%v`,
 		o.Status.StringForHuman(),
 	)
 }
