5cee89bdef6dd5196875e9aab3d77983dd2f2700
mysk <wolfbot.owner@gmail.com>
2020-04-27 15:29:09 +0900

    fix(role): assign villager if number of designated roles is less than total participants

--- domain/model/role/casting.go
+++ domain/model/role/casting.go
@@ -11,8 +11,8 @@ import (
 
 type Casting map[ID]int
 
-func ParseAndValidateCasting(str string) (Casting, error) {
-	casting, err := parseCasting(str)
+func ParseAndValidateCasting(str string, total int) (Casting, error) {
+	casting, err := parseCasting(str, total)
 	if err != nil {
 		return nil, err
 	}
@@ -24,15 +24,21 @@ func ParseAndValidateCasting(str string) (Casting, error) {
 	return casting, nil
 }
 
-func parseCasting(str string) (Casting, error) {
+func parseCasting(str string, total int) (Casting, error) {
 	result := make(Casting)
+	assigned := 0
 	for _, c := range str {
 		r, err := NewFromAbbr(Abbr(c))
 		if err != nil {
 			return nil, err
 		}
+		assigned++
 		result[r.ID]++
 	}
+
+	// 指定された役職の総数と参加者総数の差分は自動で村人に設定する
+	result[Villager] += total - assigned
+
 	return result, nil
 }
 
